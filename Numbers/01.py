0bff056f7914a32fa4247493ca21a912cb6e58ec
Rishi Rana <rishirana012354@gmail.com>
2025-09-01 22:22:05 +0530

    Numbers

---
+++ Numbers/01.py
@@ -0,0 +1,30 @@
+x=2
+y=3
+z=4
+print(x+y)
+
+print(float(40))
+
+print(x,y,z)
+
+print(1<2)
+
+print(1==2 and 2< 3)
+
+import math
+print(math.floor(3.5))  # closeset value bottom
+
+py=math.trunc(2.8)  # near the zero
+print(py)
+
+print((2+1j)*3)
+
+print(oct(64))
+print(hex(64))
+print(bin(64))
+
+import random
+random=random.randint(0,10)
+print(random)
+
+
