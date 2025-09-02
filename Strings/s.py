37b3df14b656fb55202febc1c39787f92f4ce1c7
Rishi Rana <rishirana012354@gmail.com>
2025-09-02 21:48:57 +0530

    String

---
+++ Strings/s.py
@@ -0,0 +1,19 @@
+Rajput ='Rishi Rana'
+print(Rajput)
+print(Rajput.upper())
+print(len(Rajput))
+
+first_char = Rajput[0]
+print(first_char)
+
+slice_R = Rajput[0:6]
+print(slice_R)
+
+num_list = "0123456789"
+print(num_list[:])
+print(num_list[3:])
+print(num_list[0:7:2])
+
+Rishi = "Rishi Rana Rana Rana"
+print(Rishi.count("Rana"))
+
