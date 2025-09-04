e17a2271dd94f2ae4d8d4842930a2d99e946f9e2
Rishi Rana <rishirana012354@gmail.com>
2025-09-04 22:00:04 +0530

    oop

---
+++ 08_Scopes Closure/01.py
@@ -0,0 +1,3 @@
+username = "Rishi Rana" 
+
+  
---
+++ 09_OOP/01.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def display_info(self):
+        print(f"Car Brand: {self.brand}")
+        print(f"Car Model: {self.model}")
+
+my_car = Car("Toyota", "Corolla")
+my_car.display_info()
+
+my_new_car = Car("Honda", "Civic")
+my_new_car.display_info()
