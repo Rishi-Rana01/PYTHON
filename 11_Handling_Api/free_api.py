7738413399290a0e3aa6570fd3c80c6497073e58
Rishi Rana <rishirana012354@gmail.com>
2025-09-10 19:52:36 +0530

    api up

--- 11_Handling_Api/free_api.py
+++ 11_Handling_Api/free_api.py
@@ -10,15 +10,17 @@ def fetch_random_user_freeapi():
         user_data=data["data"]
         username=user_data["login"]["username"]
         country = user_data["location"]["country"]
-        return username, country
+        coordinates = user_data["location"]["coordinates"]
+        return username, country, coordinates
     else:
         raise Exception("Failed to fetch Data")
     
 
 def main():
     try:
-       username, country= fetch_random_user_freeapi() 
+       username, country, coordinates = fetch_random_user_freeapi() 
        print(f"Username: {username} \nCountry: {country}")
+       print(f"Coordinates: Latitude {coordinates['latitude']}, Longitude {coordinates['longitude']}")
     except Exception as e:
         print(str(e))
 
